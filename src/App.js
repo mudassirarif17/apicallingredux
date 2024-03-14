7acff4d020670d887eb48ec422133e58c1aced77
mudassir-arif <mudassir@aptechgdn.net>
2024-03-14 13:06:30 +0500

    redux api calling is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { fetchTodos } from './redux/slice/todo';
+import { UseDispatch , useDispatch, useSelector } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  console.log("state" , state);
+
+  if(state.todo.isLoading){
+    return <h1>Loading....</h1>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e)=>dispatch(fetchTodos())}>Fetch todos</button>
+      {
+        state.todo.data && state.todo.data.map((e)=>(
+          <li>{e.title}</li>
+        ))
+      }
     </div>
   );
 }
 
 export default App;
+
+
+
